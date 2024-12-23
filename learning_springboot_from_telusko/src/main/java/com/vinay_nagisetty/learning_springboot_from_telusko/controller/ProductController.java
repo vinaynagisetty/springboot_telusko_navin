e6327a7761c1fe8142b89cfc7af1b400b1880725
vinay <vinaynagisetty7@gmail.com>
2024-12-23 22:33:51 +0530

    did the post api call

--- learning_springboot_from_telusko/src/main/java/com/vinay_nagisetty/learning_springboot_from_telusko/controller/ProductController.java
+++ learning_springboot_from_telusko/src/main/java/com/vinay_nagisetty/learning_springboot_from_telusko/controller/ProductController.java
@@ -3,10 +3,7 @@ package com.vinay_nagisetty.learning_springboot_from_telusko.controller;
 import com.vinay_nagisetty.learning_springboot_from_telusko.model.Product;
 import com.vinay_nagisetty.learning_springboot_from_telusko.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,11 @@ public class ProductController {
     @GetMapping("/product/{id}")
     public Product getProductById(@PathVariable int id) {
         return products.getProductById(id);
+    }
+    @PostMapping("/product")
+    public String addProduct( @RequestBody Product product) {
+         products.addProduct(product);
+         return "Product Added Successfully!";
     }
 
 }
--- learning_springboot_from_telusko/src/main/java/com/vinay_nagisetty/learning_springboot_from_telusko/service/ProductService.java
+++ learning_springboot_from_telusko/src/main/java/com/vinay_nagisetty/learning_springboot_from_telusko/service/ProductService.java
@@ -10,16 +10,19 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    List<Product> products= Arrays.asList(
+    List<Product> products= new ArrayList<>( Arrays.asList(
             new Product(1, "Laptop", 1000),
             new Product(2, "Mouse", 20),
             new Product(3, "Keyboard", 50)
-    );
+    ));
     public List<Product> getProducts() {
         return products;
     }
 
     public Product getProductById(int id) {
         return products.stream().filter(p -> p.getId() == id).findFirst().orElse(new Product(0, "Not Found", 0));
+    }
+    public void addProduct(Product product) {
+        products.add(product);
     }
 }
